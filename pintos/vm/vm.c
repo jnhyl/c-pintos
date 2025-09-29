a09bc9deb7b84dcb4c575d59b19d88cac8f42342
Junryul <farvinjr104@gmail.com>
2025-09-29 22:32:38 +0900

    [백준렬] fix: spt_insert 함수에 대한 예외처리 추가

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -91,7 +91,11 @@ struct page *spt_find_page(struct supplemental_page_table *spt, void *va) {
 /* Insert PAGE into spt with validation. */
 bool spt_insert_page(struct supplemental_page_table *spt, struct page *page) {
   ASSERT(spt != NULL);
+  ASSERT(page != NULL);
   ASSERT(pg_ofs(page->va) == 0);  // va가 page aligned인지 확인
+
+  // 같은 주소에 대해 중복 삽입이 발생하는 상황에 현재는 조용한 실패. ASSERT가
+  // 필요한가?
 
   bool succ = false;
   struct hash_elem *prev = hash_insert(&spt->page_map, &page->hash_elem);
