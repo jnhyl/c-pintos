3cfed44a7e6e712147d4fa9bd9eb4937de18ab11
Junryul <farvinjr104@gmail.com>
2025-10-01 16:10:59 +0900

    [백준렬] feat: setup_stack 함수 최소 구현

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -911,14 +911,19 @@ static bool load_segment(struct file* file, off_t ofs, uint8_t* upage,
 
 /* Create a PAGE of stack at the USER_STACK. Return true on success. */
 static bool setup_stack(struct intr_frame* if_) {
-  bool success = false;
-  void* stack_bottom = (void*)(((uint8_t*)USER_STACK) - PGSIZE);
+  void* stack_bottom = (void*)((uint8_t*)USER_STACK - PGSIZE);
 
-  /* TODO: Map the stack on stack_bottom and claim the page immediately.
-   * TODO: If success, set the rsp accordingly.
-   * TODO: You should mark the page is stack. */
-  /* TODO: Your code goes here */
+  // 1) 스택 페이지 등록 (VM_MARKER_0으로 스택 표시)
+  if (!vm_alloc_page_with_initializer(VM_ANON | VM_MARKER_0, stack_bottom, true,
+                                      NULL, NULL))
+    return false;
 
-  return success;
+  // 2) 즉시 claim
+  if (!vm_claim_page(stack_bottom)) return false;
+
+  // 3) rsp 설정
+  if_->rsp = USER_STACK;
+
+  return true;
 }
 #endif /* VM */
